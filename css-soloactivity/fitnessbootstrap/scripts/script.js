37a88547f4585f1bd8f3b51584ebe20fe242d472
JoshBegg <93288205+JoshBegg@users.noreply.github.com>
2021-11-29 15:14:12 -0700

    added files via upload

---
+++ css-soloactivity/fitnessbootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name:Josh Begg
+    File Name: script.js
+    Date: 11/29/21
+*/
+
+//jQuery for hero image to consume the header window spave
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
